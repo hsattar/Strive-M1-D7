86fe67215d3a8033feb0d55c4f7b1b17b2622b4f
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-28 17:53:52 +0100

    Added ability to send message with enter + Cleaned Code

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
-{/* <button id="change-h1" class="btn btn-primary">Change H1 Text</button>
-<button id="change-bgc" class="btn btn-primary">Change Background Colour</button>
-<button id="change-address" class="btn btn-primary">Change Footer Address</button>
-<button id="add-links-class" class="btn btn-primary">Add CSS Class To Amazon Links</button>
-<button id="toggle-img" class="btn btn-primary">Show / Hide Images</button>
-<button id="price-colour" class="btn btn-primary">Change Price Colour Randomly</button> */}
-
 let changeHeader = document.getElementById('change-h1')
-let changeBgc = document.getElementById('change-bgc')
-let changeAddress = document.getElementById('change-address')
-let addLinkClass = document.getElementById('add-links-class')
-let toggleImgs = document.getElementById('toggle-img')
-let priceColour = document.getElementById('price-colour')
-
-let header = document.getElementsByTagName('h1')[0]
-let body = document.getElementsByTagName('body')[0]
-let address = document.getElementsByTagName('address')[0]
-let links = document.getElementsByTagName('a')
-let images = document.getElementsByTagName('img')
-let prices = document.getElementsByClassName('product-price')
-
 changeHeader.onclick = () => {
+    let header = document.getElementsByTagName('h1')[0]
     header.innerText = 'You Changed The H1'
 }
 
+let changeBgc = document.getElementById('change-bgc')
 changeBgc.onclick = () => {
+    let body = document.getElementsByTagName('body')[0]
     body.style.backgroundColor = 'lightgreen'
 }
 
+let changeAddress = document.getElementById('change-address')
 changeAddress.onclick = () => {
+    let address = document.getElementsByTagName('address')[0]
     address.innerText = 'This Is A Fake Address'
 }
 
+let addLinkClass = document.getElementById('add-links-class')
 addLinkClass.onclick = () => {
+    let links = document.getElementsByTagName('a')
     for (let link of links) {
         link.classList.add('links-class')
     }
 }
 
+let toggleImgs = document.getElementById('toggle-img')
 toggleImgs.onclick = () => {
+    let images = document.getElementsByTagName('img')
     for (let img of images) {
         img.classList.toggle('toggling-image')
     }
 }
 
+let priceColour = document.getElementById('price-colour')
 let btnclicks = 0
+let prices = document.getElementsByClassName('product-price')
 priceColour.onclick = () => {
     let colours = ['red', 'blue', 'green', 'yellow', 'orange', 'grey']
     for (let i = 0; i < prices.length; i++) {
@@ -56,9 +47,17 @@ let msgArea = document.getElementById('message-area')
 let messageInput = document.getElementById('message-box')
 let send = document.getElementById('send-btn')
 
+messageInput.onkeyup = (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        send.click()
+    }
+}
+
 send.onclick = () => {
     let newMsg = document.createElement('p')
     newMsg.innerText = messageInput.value
     msgArea.appendChild(newMsg)
     messageInput.value = ''
+    messageInput.focus()
 }
