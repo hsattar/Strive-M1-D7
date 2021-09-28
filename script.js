0355ea163a9cf0eba4c51cd935bb59cc8e9908c4
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-28 15:26:31 +0100

    Added Class To Links + Change Price Colour

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ let priceColour = document.getElementById('price-colour')
 let header = document.getElementsByTagName('h1')[0]
 let body = document.getElementsByTagName('body')[0]
 let address = document.getElementsByTagName('address')[0]
+let links = document.getElementsByTagName('a')
 let images = document.getElementsByTagName('img')
+let prices = document.getElementsByClassName('product-price')
 
 changeHeader.onclick = () => {
     header.innerText = 'You Changed The H1'
@@ -29,8 +31,20 @@ changeAddress.onclick = () => {
     address.innerText = 'This Is A Fake Address'
 }
 
+addLinkClass.onclick = () => {
+    for (let link of links) {
+        link.classList.add('random-class')
+    }
+}
+
 toggleImgs.onclick = () => {
-    for (img of images) {
+    for (let img of images) {
         img.classList.toggle('toggling-image')
+    }
+}
+priceColour.onclick = () => {
+    let colours = ['red', 'blue', 'green']
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].style.color = colours[i]
     }
 }
